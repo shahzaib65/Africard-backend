27c7ec72b2b6c296c1ac8718c50d8cebb90dedaa
@Faimasoft786 <FatimaSoft786@gmail.com>
2023-11-05 08:09:43 +0500

    first commit

---
+++ db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+require("dotenv").config();
+
+mongoose.set("strictQuery", false);
+const connectToMongo = () => {
+    mongoose.connect(process.env.MONGO_URI,{
+    }).then(()=> {
+        console.log(`connection established`);
+    }).catch((err) => console.log(err));
+}
+
+module.exports = connectToMongo
+
+//https://github.com/shahzaib65/Africard.git
